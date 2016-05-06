4503200bc6153df0fee951e0d146b227b5d3b829
Daniel Kesler <danielckesler@gmail.com>
2016-05-06 12:37:56 -0600

    Update package.json

--- src/cpr-autoscale-input.js
+++ src/cpr-autoscale-input.js
@@ -7,19 +7,19 @@ var html = function(size, family) {
 
 var CanopyAutoscaleInput = React.createClass({
 
-  componentWillReceiveProps: function(props) {
-    this.measureAndSize(props.value);
-  },
+	componentWillReceiveProps: function(props) {
+		this.measureAndSize(props.value);
+	},
 
-  componentDidMount: function() {
-    this.measureAndSize();
-  },
+	componentDidMount: function() {
+		this.measureAndSize();
+	},
 
-  measureAndSize: function(text) {
-    text = text || this.input.value;
-    if (this.props.inputAttrs && this.props.inputAttrs.placeholder) text = text || this.props.inputAttrs.placeholder;
-    text = text.length < 2 ? 'HI' : text;
-    text = text.replace(/\s/g, "_");
+	measureAndSize: function(text) {
+		text = text || this.input.value;
+		if (this.props.inputAttrs && this.props.inputAttrs.placeholder) text = text || this.props.inputAttrs.placeholder;
+		text = text.length < 2 ? 'HI' : text;
+		text = text.replace(/\s/g, "_");
 		let inputStyle = getComputedStyle(this.input, null);
 
 		let wrapperDiv = document.createElement("div");
@@ -32,25 +32,25 @@ var CanopyAutoscaleInput = React.createClass({
 
 		this.input.parentNode.insertBefore(wrapperDiv, this.input);
 
-    this.input.style.width = wrapperDiv.offsetWidth + 6 + 'px';
+		this.input.style.width = wrapperDiv.offsetWidth + 6 + 'px';
 		this.input.parentNode.removeChild(wrapperDiv);
-  },
-  handleChange: function(property, event) {
-    this.measureAndSize();
-    // Call parent callback
-    this.props.onChange(property, event);
-  },
+	},
+	handleChange: function(property, event) {
+		this.measureAndSize();
+		// Call parent callback
+		this.props.onChange(property, event);
+	},
 
-  render: function() {
-    return (
+	render: function() {
+		return (
 			<input
 				ref={ref => this.input = ref}
 				name={this.props.propName}
 				value={this.props.value}
 				onChange={this.handleChange.bind(this, this.props.propName)}
 				readOnly={this.props.readOnly} {...this.props.inputAttrs}/>
-    );
-  }
+		);
+	}
 });
 
 if (window && !window.CanopyAutoscaleInput) window.CanopyAutoscaleInput = CanopyAutoscaleInput;
